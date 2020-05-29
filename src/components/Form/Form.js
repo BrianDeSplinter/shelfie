580fe5d04d3fa3ecc7242c43cfb7ebb8252150bb
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-29 13:07:57 -0600

    Added input handling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,9 +2,60 @@ import React, {Component} from 'react'
 import './Form.css'
 
 class Form extends Component {
+    constructor () {
+        super()
+        this.state = {
+            name: '',
+            price: 0,
+            imgurl: ''
+        }
+    }
+
+    handleNameChange(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handelPriceChange(e) {
+        this.setState({
+            price: e.target.value
+        })
+    }
+
+    handleImageChange(e) {
+        this.setState({
+            imgurl: e.target.value
+        })
+    }
+
+    postNewProduct() {
+
+    }
+
+    cancelInput(){
+        this.setState({
+            name: '',
+            price: 0,
+            imgurl: ''
+        })
+    }
+
+
     render() {
         return(
-            <div>Form</div>
+            <div>
+                <div>Form</div>
+                <div className='inputs'>
+                    <input onChange={(e) => this.handleNameChange(e)} placeholder='Name'/>
+                    <input onChange={(e) => this.handlePriceChange(e)} placeholder='Price'/>
+                    <input onChange={(e) => this.handleImageChange(e)} placeholder='Image URL'/>
+                </div>
+                <div className='buttons'>
+                    <button onClick={()=> this.cancelInput()}>Cancel</button>
+                    <button>Add to Inventory</button>
+                </div>
+            </div>
         )
     }
 }
