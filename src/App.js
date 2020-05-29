324b61962e8e7c403181b5b8fba7007a5bca7f8d
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-29 13:07:20 -0600

    Added Temp Products

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard'
 import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <Dashboard/>
-      <Form/>
-      <Header/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      products: [
+        {
+          name: 'Stuff',
+          price: 10.99,
+          imgurl: 'https://www.responsesource.com/content/uploads/2016/06/Stuff.jpg'
+        },
+        {
+          name: 'Things',
+          price: 4.99,
+          imgurl: 'https://cupofjo.com/wp-content/uploads/2013/04/maddie-on-things.jpg'
+        },
+        {
+          name: 'XBox OneX',
+          price: 399.00,
+          imgurl: 'https://th.bing.com/th/id/OIP.T0nii6LBXeiPPRQjjkCV8wHaEK?pid=Api&rs=1'
+        }
+      ]
+    }
+  }
+  
+  
+  
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Dashboard products={this.state.products}/>
+        <Form/>
+      </div>
+    );
+  }
 }
 
 export default App;
