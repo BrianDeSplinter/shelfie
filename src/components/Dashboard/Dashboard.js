df34d69d9916ab415b5721f92b6a4c7418c254c7
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-29 16:57:07 -0600

    Added delete method

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react'
 import './Dashboard.css'
 import Product from '../Product/Product'
+import axios from 'axios'
 
 class Dashboard extends Component {
+    deleteProduct(id) {
+        axios.delete(`/api/product/${id}`).then(res => {
+            this.props.componentDidMount()
+        })
+    }
+
     render(){
         const productMap = this.props.products.map(e => (
             <Product 
@@ -10,6 +17,7 @@ class Dashboard extends Component {
                 name={e.product_name}
                 price={e.price}
                 imgurl={e.product_image}
+                delete={this.deleteProduct}
             />
         ))
         return(
