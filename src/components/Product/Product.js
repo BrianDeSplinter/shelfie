0c350197235d7d2e4eeb9392b53af17a2f6e7a47
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-29 15:41:15 -0600

    Added alt for img

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,7 +5,7 @@ class Product extends Component{
     render() {
         return(
             <div className='product'>
-                <img src={this.props.imgurl}/>
+                <img src={this.props.imgurl} alt='failed to load :('/>
                 <div className='productInfo'>
                     <h4>{this.props.name}</h4>
                     <h4>{this.props.price}</h4>
