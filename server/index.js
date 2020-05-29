0951a8a4985242835c0985d2e980746e68f0dcd3
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-29 11:22:14 -0600

    Db setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
+require('dotenv').config()
 const express = require('express')
+const massive = require('massive')
 const app = express()
 const ctrl = require('./controller')
 const SERVER_PORT = 4000
+const {CONNECTION_STRING} = process.env
 
 app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
 
 
 app.listen(SERVER_PORT, () =>
