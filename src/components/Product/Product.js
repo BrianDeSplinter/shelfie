68306cb66314f52075b39a7e3f2527d4eb3775ea
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-16 19:32:47 -0600

    Working on Delete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,14 +2,17 @@ import React, {Component} from 'react'
 import './Product.css'
 
 class Product extends Component{
+       
     render() {
+        // console.log(this.props)
         return(
             <div className='product'>
-                <img src={this.props.imgurl} alt='failed to load :('/>
+                <img src={this.props.imgUrl} alt='failed to load :('/>
                 <div className='productInfo'>
                     <h4>{this.props.name}</h4>
                     <h4>{this.props.price}</h4>
-                    <button onClick={this.props.delete(this.props.key)}>Delete</button>
+                    <button onClick={() => this.props.delete(this.props.id)}>Delete</button>
+                    <button onClick={() => {}}>Edit</button>
                 </div>
             </div>
         )
