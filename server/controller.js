4fabfa10a108b5f595cf5c86cd764349f67071bb
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-22 14:08:34 -0600

    added edit controller

--- server/controller.js
+++ server/controller.js
@@ -25,5 +25,16 @@ module.exports = {
         db.delete_product(id)
         .then(() => res.sendStatus(200))
         .catch(err => res.status(500).send(err))
+    },
+
+    editProduct: (req, res) => {
+        const {id} = req.params
+        const{product_name, product_image, price} = req.body
+
+        const db = req.app.get('db')
+
+        db.edit_product(id, product_name, product_image, price)
+        .then(product => res.status(200).send(product))
+        .catch(err => res.status(500).send(err))
     }
 }
